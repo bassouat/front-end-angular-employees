440b71fcede29ddc93d2932b1f4f4d1d752eb1c6
Basseydou Ouattara <bassouat8@gmail.com>
2022-09-21 09:31:38 +0200

    On crée la classe et le composant

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {Employee} from "../employee";
+import {EmployeeService} from "../employee.service";
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+  employees: Employee[] = [];
+
+  constructor(private employeeService:EmployeeService) {
+  }
+
+  ngOnInit(): void {
+    /*this.employees = [{"id": 1, "firstName": "Bouchra", "lastName": "Bouffon", "emailId": "bass1@gmail.com"},
+      {"id": 2, "firstName": "Mateo", "lastName": "Ali", "emailId": "stark1@yahoo.fr"},
+      {"id": 3, "firstName": "Yan", "lastName": "Murphy", "emailId": "Murphy@hotmail.fr"},
+      {"id": 4, "firstName": "Mathias", "lastName": "Quentin", "emailId": "Robert@live.fr"}];*/
+    this.getEmployeeList();
+
+
+
+  }
+
+  getEmployeeList():void {
+    this.employeeService.getEmployeesList().subscribe(data =>{this.employees=data});
+
+  }
+
+
+}
---
+++ src/app/employee.ts
@@ -0,0 +1,7 @@
+export class Employee {
+  id!: number;
+  firstname!: string;
+  lastName!: string;
+  emailId!: string;
+
+}
